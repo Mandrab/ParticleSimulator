3df43aa7a0e6f87fbf7aeb6a0ee44b14eb0f15e9
Ylenia Battistini <ylenia.battistini@studio.unibo.it>
2020-04-04 14:50:38 +0200

    fix error -> OK collision

--- ass_01/src/main/Body.java
+++ ass_01/src/main/Body.java
@@ -130,10 +130,14 @@ public class Body {
     	double v1x = v1.getX() - x12dx*fact12;
     	double v1y = v1.getY() - x12dy*fact12;
     
-    	double fact21=-fact12;
-    	double v2x= -v1.getX()-(-x12dx*fact21);
-    	double v2y= -v1.getY()-(-x12dy*fact21);
-
+    	double x21dx = x2.getX() - x1.getX();
+    	double x21dy = x2.getY() - x1.getY();
+    	double v21dx = v2.getX() - v1.getX();
+    	double v21dy = v2.getY() - v1.getY();    	
+    	double fact21 = (x21dx*v21dx + x21dy*v21dy) / (x21dx*x21dx + x21dy*x21dy);    	
+    	double v2x = v2.getX() - x21dx*fact21;
+    	double v2y = v2.getY() - x21dy*fact21;
+    	
     	b1.changeVel(v1x, v1y);
     	b2.changeVel(v2x, v2y);
     }
